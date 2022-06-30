2f0584416125e348085c4c31e68c54191d208cd8
Kiara <kiarabroekhuizen1@hotmail.com>
2022-06-30 16:59:25 +0200

    join msg works

--- public/script.js
+++ public/script.js
@@ -2,63 +2,65 @@ import { getNewCharacter } from "./modules/character.js"
 
 const socket = io()
 let messages = document.querySelector('section ul')
-let input = document.querySelector('input')
 let form = document.querySelector('#chat')
-const users = document.querySelector('.users > ul ')
-let nameInput = document.querySelector('#name')
+let gamePage = document.querySelector('.game')
 const answerInput = document.querySelector('#answerInput')
 
-//If new user, loop through the users array and display them
-socket.on('user connect', user => {
-    console.log(users)
-    users.innerHTML = ''
-    user.map((user) => {
-        users.appendChild(Object.assign(document.createElement('li'), { textContent: user + ' has joined! 🥳' }))
+
+
+if (gamePage) {
+    // when user connects, show message
+    socket.on('user connect', userName => {
+        if (userName) {
+            console.log(userName)
+            const item = document.createElement('li');
+            item.classList.add('joined-msg')
+            item.textContent = `${userName} has joined!`;
+            messages.appendChild(item);
+
+
+        }
+
     })
-})
-
-
-socket.on('new character', (data) => {
-    getNewCharacter(data)
-})
 
 
 
-socket.on('message', (msg) => {
-    const item = document.createElement('li');
-    item.textContent = `${msg.message}`;
-    messages.appendChild(item);
-    window.scrollTo(0, document.body.scrollHeight);
-});
-
-socket.on('user connect', msg => {
-    console.log(msg)
-    const joinMessage = document.createElement('li');
-    joinMessage.textContent = msg;
-    messages.appendChild(joinMessage);
+    socket.on('new character', (data) => {
+        getNewCharacter(data)
+    })
 
 
-})
-//check if there's an input > emit answer
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    if (answerInput.value) {
-        socket.emit('message', {
-            message: answerInput.value,
-            // username: username
-        });
-        socket.emit('answer', answerInput.value)
-        answerInput.value = '';
 
-    }
-});
+    socket.on('message', (msg) => {
+        const item = document.createElement('li');
+        item.textContent = `${msg.message}`;
+        messages.appendChild(item);
+        window.scrollTo(0, document.body.scrollHeight);
+    });
 
 
-//Show alert if answer is correct
-socket.on('good answer', (msg) => {
-    const item = document.createElement('li');
-    item.textContent = 'Het antwoord is geraden! Het antwoord is ' + msg.toString()
-    messages.appendChild(item);
-    window.scrollTo(0, document.body.scrollHeight);
+    //check if there's an input > emit answer
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        if (answerInput.value) {
+            socket.emit('message', {
+                message: answerInput.value,
+                // username: username
+            });
+            socket.emit('answer', answerInput.value)
+            answerInput.value = '';
 
-})
+        }
+    });
+
+
+    //Show message if answer is correct
+    socket.on('good answer', (msg) => {
+        const item = document.createElement('li');
+        item.classList.add('good-answer')
+        item.textContent = 'Het antwoord is geraden! Het antwoord is ' + msg.toString()
+        messages.appendChild(item);
+        window.scrollTo(0, document.body.scrollHeight);
+
+    })
+}
--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const port = process.env.PORT || 4242
 const server = createServer(app)
 export const io = new Server(server)
 
+let username;
 // set templating engine
 app.set('view engine', 'ejs');
 //w here the templates are stored
@@ -23,18 +24,21 @@ app.get('/', (req, res) => {
     res.render('login', { randomElement })
 })
 app.get('/game', (req, res) => {
-    const userName = req.query.userName;
+    username = req.query.userName;
     // console.log(userName)
-    res.render('index', { userName })
+    res.render('index', { username })
 })
+
+// global variables
 let characterData;
 let randomElement;
 let users = [];
 let game = 0;
 
-
+// array with characters that are well known
 const characters = ['Thor', 'Punisher', 'Daredevil', 'Hulk', 'Deadpool', 'Captain America', 'Doctor Strange']
 
+// get Data function
 const getData = async () => {
 
     try {
@@ -66,6 +70,7 @@ const getData = async () => {
     }
 }
 
+// setup data
 getData()
     .then(() => console.log('Loading the data..', randomElement[game].name))
     .then(() => {
@@ -81,17 +86,18 @@ getData()
 
 // setting up socket connection
 io.on('connection', (socket) => {
-    // global variable for connecting name
-    let userName;
-    io.emit('user connect', users);
 
-    // new user
-    socket.on('user connect', (username) => {
-        userName = username;
-        users.push(userName);
+    socket.on('user connect', (userName) => {
+        io.emit('user connect', { username })
         console.log(username)
-        io.emit('user connect', users)
-    })
+    });
+
+    users.push({
+        username: username
+    });
+    // global variable for connecting name
+    io.emit('user connect', (username));
+    console.log('user connected')
 
     // emmitting filtered data from api
     io.emit('new character', characterData)
@@ -106,6 +112,7 @@ io.on('connection', (socket) => {
         if (guess.includes(goodAnswer)) {
             io.emit('good answer', characterName);
 
+            // for next character if answer is correct
             if (game >= randomElement.length - 1) {
                 game = 0;
                 getData()
@@ -117,13 +124,14 @@ io.on('connection', (socket) => {
                 name: randomElement[game].name,
                 url: randomElement[game].thumbnail.path + '/portrait_incredible.' + randomElement[game].thumbnail.extension
             }
+            // call for next character 
             io.emit('new character', characterData)
             console.log('good answer', game)
         }
     });
 
 
-
+    // setup for messages
     socket.on('message', (message) => {
         io.emit('message', message)
     })
